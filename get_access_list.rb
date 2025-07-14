97b93365d2108c324f54dd72a2384e18551f453a
bradleyb74 <bbarton@sitesentinel.com.au>
2025-07-14 17:12:31 +0800

    Add files to GitHub

---
+++ get_access_list.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require_relative 'config.rb' # load site config
+require 'httparty'
+
+api_url = "https://www.sitesentinel.com.au/api/gates/access_tokens"
+
+response = HTTParty.get(api_url, {
+  headers: {"Authorization" => "Token #{GATE_ID}" }
+})
+
+if response.code == 200
+  File.write("/home/ubuntu/site-sentinel-box-usb-readers/access_list.txt", response.body)
+else
+  puts "ERROR!"
+end
